f52926394a3166ee343f71c732e398df59abd6f9
JoseLuisFenollPardo <joseluis.fenollp@gmail.com>
2024-11-13 19:02:25 +0100

    método registrarUsuario() del adaptador terminado

--- src/main/java/umu/tds/persistencia/AdaptadorUsuarioTDS.java
+++ src/main/java/umu/tds/persistencia/AdaptadorUsuarioTDS.java
@@ -1,5 +1,6 @@
 package umu.tds.persistencia;
 
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.Arrays;
 
@@ -7,12 +8,17 @@ import beans.Entidad;
 import beans.Propiedad;
 import tds.driver.FactoriaServicioPersistencia;
 import tds.driver.ServicioPersistencia;
+import umu.tds.dominio.Contacto;
+import umu.tds.dominio.ContactoIndividual;
+import umu.tds.dominio.Grupo;
 import umu.tds.dominio.Usuario;
 
 public class AdaptadorUsuarioTDS implements UsuarioDAO {
 	
 	private static ServicioPersistencia servPersistencia;
 	private static AdaptadorUsuarioTDS unicaInstancia = null;
+	
+	DateTimeFormatter formato = DateTimeFormatter.ofPattern("dd/MM/yyyy");
 	
 	public static AdaptadorUsuarioTDS getUnicaInstancia() {
 		if (unicaInstancia == null) {
@@ -36,26 +42,43 @@ public class AdaptadorUsuarioTDS implements UsuarioDAO {
 		} catch (Exception e) {}
 		if (eUsuario != null) return;
 		
+		//Registramos atributos que son objetos
+		AdaptadorContactoIndividualTDS adaptadorContactoIndividual = AdaptadorContactoIndividualTDS.getInstancia();
+		AdaptadorGrupoTDS adaptadorGrupo = AdaptadorGrupoTDS.getInstancia();
+		for (Contacto contacto: user.getContactos()) {
+			if(contacto instanceof ContactoIndividual) {
+				adaptadorContactoIndividual.registrarContactoIndividual((ContactoIndividual) contacto);
+			}else if (contacto instanceof Grupo) {
+				adaptadorGrupo.registrarGrupo((Grupo) contacto);
+			}
+		}
+		
 		//Creamos una entidad producto
 		eUsuario = new Entidad();
 		
+		//Asignamos tipo
 		eUsuario.setNombre("usuario");
+		
+		//Registramos atributos
+		String fechaNacim = user.getFechaNacimiento().format(formato);
 		eUsuario.setPropiedades(new ArrayList<Propiedad>(Arrays.asList(
 				new Propiedad("nombre", user.getNombre()),
 				new Propiedad("apellidos", user.getApellidos()),
 				new Propiedad("numTlf", String.valueOf(user.getNumTlf())),
 				new Propiedad("password", user.getPassword()),
-				new Propiedad("estado", user.getEstado())
-				//new Propiedad("fechaNacimiento", user.getFechaNacimiento()),
-				
-				
-				
-				
-				
-				
+				new Propiedad("fotoPerfil", String.valueOf(user.getFotoPerfil().getDescription())),
+				new Propiedad("estado", user.getEstado()),
+				new Propiedad("fechaNacimiento", fechaNacim),
+				new Propiedad("email", user.getEmail()),
+				new Propiedad("isPremium", String.valueOf(user.isPremium()))
 				)));
 		
+		// registrar entidad cliente
+		eUsuario = servPersistencia.registrarEntidad(eUsuario);
+		// asignar identificador unico al usuario, aprovechando el que genera el servicio de persistencia
+		user.setCodigo(eUsuario.getId());
 	}
+	
 
 	@Override
 	public void borrarUsuario(Usuario user) {
