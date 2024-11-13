25db0915fe0cb4ace318bc88ee7d6b87779500bb
JoseLuisFenollPardo <joseluis.fenollp@gmail.com>
2024-11-13 19:01:24 +0100

    añadiendo método setCodigo()

--- src/main/java/umu/tds/dominio/Usuario.java
+++ src/main/java/umu/tds/dominio/Usuario.java
@@ -61,6 +61,9 @@ public class Usuario {
 	public int getCodigo() {
 		return codigo;
 	}
+	public void setCodigo(int codigo) {
+		this.codigo = codigo;
+	}
 	public String getNombre() {
 		return nombre;
 	}
