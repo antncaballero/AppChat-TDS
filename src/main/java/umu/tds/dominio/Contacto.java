7ec3fa39ae79b5f41adc1d93c83f4e26dd9d4357
JoseLuisFenollPardo <joseluis.fenollp@gmail.com>
2024-11-14 14:10:02 +0100

    Nuevo atributo de contacto, codigo

--- src/main/java/umu/tds/dominio/Contacto.java
+++ src/main/java/umu/tds/dominio/Contacto.java
@@ -7,16 +7,25 @@ import javax.swing.ImageIcon;
 
 public abstract class Contacto {
 	
+	private int codigo;
 	private String nombre;
 	private List<Mensaje> listaMensajes;
 	
 	public Contacto(String nombre) {
+		this.codigo = 0;
 		this.nombre = nombre;
 		this.listaMensajes = new LinkedList<Mensaje>();
 	}
 	
 	public String getNombre() {
 		return nombre;
+	}
+	public int getCodigo() {
+		return codigo;
+	}
+	
+	public void setCodigo(int codigo) {
+		this.codigo = codigo;
 	}
 	
 	public abstract ImageIcon getFoto();
